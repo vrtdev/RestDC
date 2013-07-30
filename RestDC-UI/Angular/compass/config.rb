db218d919b54f23a6d57223693b5a120d0c85f67
Michael Seghers <michael.seghers@ida-mediafoundry.be>
2013-07-30 17:39:02 +0200

    Mavenized the UI modules

--- RestDC-UI/Angular/compass/config.rb
+++ RestDC-UI/Angular/compass/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "../target/stylesheets"
+sass_dir = "../src/main/sass"
+images_dir = "../src/main/images"
+javascripts_dir = "../src/main/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
