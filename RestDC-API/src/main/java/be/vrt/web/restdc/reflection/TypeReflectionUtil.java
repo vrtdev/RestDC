26f1fa44e6765b98b5929f98df31db33bd1f550f
Michael Seghers <michael.seghers@ida-mediafoundry.be>
2013-09-24 13:25:13 +0200

    Refactored relection utility

--- RestDC-API/src/main/java/be/vrt/web/restdc/reflection/TypeReflectionUtil.java
+++ RestDC-API/src/main/java/be/vrt/web/restdc/reflection/TypeReflectionUtil.java
@@ -6,60 +6,25 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Mike Seghers
  */
 public final class TypeReflectionUtil {
+    private static List<ReflectiveTypeHandler<?>> reflectiveTypeHandlers;
+
+    static {
+        reflectiveTypeHandlers = new ArrayList<>();
+        reflectiveTypeHandlers.add(new ClassHandler());
+        reflectiveTypeHandlers.add(new ParameterizedTypeHandler());
+        reflectiveTypeHandlers.add(new TypeVariableTypeHandler());
+        reflectiveTypeHandlers.add(new WildcardTypeTypeHandler());
+    }
+
     private TypeReflectionUtil() {
-
-    }
-
-    private static String getActualTypeName(final java.lang.reflect.Type type) {
-        String result = null;
-
-        if (type instanceof Class) {
-            result = ((Class) type).getSimpleName();
-        } else if (type instanceof ParameterizedType) {
-            result = getActualTypeName(((ParameterizedType) type).getRawType());
-        } else if (type instanceof TypeVariable) {
-            TypeVariable typeVar = (TypeVariable) type;
-            java.lang.reflect.Type[] bounds = typeVar.getBounds();
-            result = typeVar.getName();
-            if (!bounds[0].equals(Object.class)) {
-                result += " extends " + getActualTypeName(bounds[0]);
-            }
-        } else if (type instanceof WildcardType) {
-            WildcardType wildcardType = (WildcardType) type;
-            java.lang.reflect.Type[] upperBounds = wildcardType.getUpperBounds();
-            java.lang.reflect.Type[] lowerBounds = wildcardType.getLowerBounds();
-            if (lowerBounds.length > 0) {
-                result = "super " + getActualTypeName(lowerBounds[0]);
-            } else {
-                result = "extends " + getActualTypeName(upperBounds[0]);
-            }
-
-        }
-        return result;
-    }
-
-    private static String[] getActualGenericTypeName(final java.lang.reflect.Type type) {
-        String[] result = null;
-        if (type instanceof ParameterizedType) {
-            List<String> strings = new ArrayList<>();
-            for (java.lang.reflect.Type actType : ((ParameterizedType) type).getActualTypeArguments()) {
-                strings.add(getActualTypeName(actType));
-            }
-            result = strings.toArray(new String[strings.size()]);
-        } else if (type instanceof Class) {
-            List<String> strings = new ArrayList<>();
-            for (TypeVariable tv : ((Class) type).getTypeParameters()) {
-                strings.add(getActualTypeName(tv));
-            }
-            result = strings.toArray(new String[strings.size()]);
-        }
-        return result;
     }
 
     /**
@@ -77,13 +42,43 @@ public final class TypeReflectionUtil {
         return builder.build();
     }
 
+    private static <T extends java.lang.reflect.Type> String getActualTypeName(final T type) {
+        ReflectiveTypeHandler<T> reflectiveTypeHandler = getReflectiveTypeHandler(type);
+        String result = null;
+        if (reflectiveTypeHandler != null) {
+            result = reflectiveTypeHandler.getActualTypeName(type);
+        }
+        return result;
+    }
+
+    private static <T extends java.lang.reflect.Type>  String[] getActualGenericTypeName(final T type) {
+        ReflectiveTypeHandler<T> reflectiveTypeHandler = getReflectiveTypeHandler(type);
+        String[] result = null;
+        if (reflectiveTypeHandler != null) {
+            result = reflectiveTypeHandler.getActualGenericTypeName(type);
+        }
+        return result;
+    }
+
+    private static <T extends java.lang.reflect.Type> ReflectiveTypeHandler<T> getReflectiveTypeHandler(T type) {
+        ReflectiveTypeHandler<T> result = null;
+        for (ReflectiveTypeHandler<?> handler : reflectiveTypeHandlers) {
+            if (handler.accept(type)) {
+                result = (ReflectiveTypeHandler<T>)handler;
+                break;
+            }
+        }
+        return result;
+    }
+
 
     private interface ReflectiveTypeHandler<T extends java.lang.reflect.Type> {
         String getActualTypeName(T type);
         String[] getActualGenericTypeName(T type);
+        boolean accept(java.lang.reflect.Type type);
     }
 
-    private class ClassHandler implements ReflectiveTypeHandler<Class<?>> {
+    private static class ClassHandler implements ReflectiveTypeHandler<Class<?>> {
         @Override
         public String getActualTypeName(final Class<?> type) {
             return type.getSimpleName();
@@ -97,9 +92,14 @@ public final class TypeReflectionUtil {
             }
             return strings.toArray(new String[strings.size()]);
         }
+
+        @Override
+        public boolean accept(java.lang.reflect.Type type) {
+            return (type instanceof Class);
+        }
     }
 
-    private class ParameterizedTypeHandler implements ReflectiveTypeHandler<ParameterizedType> {
+    private static class ParameterizedTypeHandler implements ReflectiveTypeHandler<ParameterizedType> {
         @Override
         public String getActualTypeName(final ParameterizedType type) {
             return TypeReflectionUtil.getActualTypeName(type.getRawType());
@@ -112,6 +112,58 @@ public final class TypeReflectionUtil {
                 strings.add(TypeReflectionUtil.getActualTypeName(actType));
             }
             return strings.toArray(new String[strings.size()]);
+        }
+
+        @Override
+        public boolean accept(java.lang.reflect.Type type) {
+            return (type instanceof ParameterizedType);
+        }
+    }
+
+    private static class TypeVariableTypeHandler implements ReflectiveTypeHandler<TypeVariable<?>> {
+        @Override
+        public String getActualTypeName(final TypeVariable<?> type) {
+            java.lang.reflect.Type[] bounds = type.getBounds();
+            String result = type.getName();
+            if (!bounds[0].equals(Object.class)) {
+                result += " extends " + TypeReflectionUtil.getActualTypeName(bounds[0]);
+            }
+            return result;
+        }
+
+        @Override
+        public String[] getActualGenericTypeName(TypeVariable<?> type) {
+            return new String[0];
+        }
+
+        @Override
+        public boolean accept(java.lang.reflect.Type type) {
+            return type instanceof TypeVariable;
+        }
+    }
+
+    private static class WildcardTypeTypeHandler implements  ReflectiveTypeHandler<WildcardType> {
+        @Override
+        public String getActualTypeName(WildcardType type) {
+            java.lang.reflect.Type[] upperBounds = type.getUpperBounds();
+            java.lang.reflect.Type[] lowerBounds = type.getLowerBounds();
+            String result;
+            if (lowerBounds.length > 0) {
+                result = "super " + TypeReflectionUtil.getActualTypeName(lowerBounds[0]);
+            } else {
+                result = "extends " + TypeReflectionUtil.getActualTypeName(upperBounds[0]);
+            }
+            return result;
+        }
+
+        @Override
+        public String[] getActualGenericTypeName(WildcardType type) {
+            return new String[0];
+        }
+
+        @Override
+        public boolean accept(java.lang.reflect.Type type) {
+            return type instanceof WildcardType;
         }
     }
 
